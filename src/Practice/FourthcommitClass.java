dde9f3fcaece3c0796dade662e1a035c89e14a28
Sharifullah Faqery <Shariffaqery@gmail.com>
2021-08-17 00:00:13 -0400

    Created an array and printed all the elements

--- src/Practice/FourthcommitClass.java
+++ src/Practice/FourthcommitClass.java
@@ -3,7 +3,12 @@ package Practice;
 public class FourthcommitClass {
 
 	public static void main(String[] args) {
-		
+
+		int[] arr = { 10, 20, 30, 40, 50, 60 };
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(arr[i]);
+		}
 
 	}
 
