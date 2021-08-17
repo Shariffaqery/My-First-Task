bc762e0f6e9949711cf2cdc8d72ef065a9b0f674
Sharifullah Faqery <Shariffaqery@gmail.com>
2021-08-17 01:28:48 -0400

    Created another class after remote connection

---
+++ src/Practice/Practice.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I have just made another class after I connected my remote");
+
+	}
+
+}
